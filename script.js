1c5d862c8e6cc943443ac3c54b6bd435dd815c73
Dale Ramdeen <dramdeen1@gmail.com>
2020-02-27 08:21:29 -0500

    quick push changes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//The `classNames` variable can be used to link any elements you create in js with the associated CSS class names.
 const classNames = {
   TODO_ITEM: 'todo-container',
   TODO_CHECKBOX: 'todo-checkbox',
@@ -5,10 +6,79 @@ const classNames = {
   TODO_DELETE: 'todo-delete',
 }
 
+//HTML elements that you'll need to update when creating new TODOs
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
-
+let todoList = []
+//This gets executed when creating a new TODO. You should replace the `alert()` call with logic to create new TODOs.
 function newTodo() {
-  alert('New TODO button clicked!')
+  let item = prompt("Enter a TODO item:")  //promt user
+  todoList.push(item) //add item to todoList object
+  let node = document.createElement("LI") //create <li></li>
+  let listitem = document.createTextNode(item) // put the task list item in the <li>
+  let itemcount = todoList.length // get total num of tasks
+
+  //checkbox 
+  var form = document.createElement('form');
+  var checkbox = document.createElement('input');  //create the texbox html
+  checkbox.type = "checkbox";  //attributes of the checkbox
+  //checkbox.name = "type" + itemcount; //attributes of the checkbox
+  checkbox.id = itemcount;
+  checkbox.value = itemcount;
+  checkbox.onclick = checked ;
+
+  node.appendChild(form);
+  node.appendChild(checkbox); 
+  node.appendChild(listitem)
+  document.getElementById("todo-list").appendChild(node)
+  document.getElementById("item-count").innerText=itemcount
+
+  let box = event.target;
+  //console.log(box.id)
+  //console.log(checkbox.checked)
 }
+
+
+
+
+// function checked(event) { 
+//   let checkbox = event.target;
+//   console.log("+++++++++")
+//   console.log(checkbox)
+//   console.log("-----")
+//   for(i=0; i <= todoList.length; i++){
+//       let name = "box"+i
+//       //let box = document.getElementById('todo-list').getElementsByTagName('input');
+//       let box = document.getElementById(i)
+//       //console.log(box[i])
+//       console.log(box)
+//       // if (box.checked){
+//       //   alert("box checked")
+//       // }
+//   }
+  
+// }
+
+function checked() { 
+  console.log(getTotal())
+  let unchecked = getTotal()
+  document.getElementById('unchecked-count').innerText=unchecked
+}
+
+
+function getTotal(){
+
+  let checkarr = document.getElementsByTagName("input")  
+  let total = 0 
+  for (i =0; i < checkarr.length; i++){
+    
+    if (checkarr[i].type == 'checkbox' && checkarr[i].checked === false) {
+      total++
+    }
+
+  }
+  return total
+}
+
+ 
